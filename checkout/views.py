9614a95bca53df15687fcc7227dbd473e5b9b7f1
Conor Walsh <conorwalsh0703@gmail.com>
2019-08-02 12:38:06 +0100

    updated checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ stripe.api_key = settings.STRIPE_SECRET
 
 @login_required()
 def checkout(request):
+
+
     if request.method =="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
